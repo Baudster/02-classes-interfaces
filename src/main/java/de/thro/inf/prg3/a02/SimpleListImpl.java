cd42f7bd4cdf07c1735c389113da5c570b5702ad
baudster <bauderse68422@th-nuernberg.de>
2019-04-03 22:30:52 +0200

    Erster Entwurf und Struktur

--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -1,11 +1,53 @@
 package de.thro.inf.prg3.a02;
 
+import java.util.Iterator;
+
 /**
  * @author Peter Kurfer
  * Created on 10/6/17.
  */
-public class SimpleListImpl implements SimpleList {
+public class SimpleListImpl implements SimpleList, Iterable {
 
-	// TODO: Implement the required methods.
+    Element head;
 
+    // SimpleList Methoden
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public SimpleList filter(SimpleFilter filter) {
+        return null;
+    }
+
+    // Iterable Methoden
+    @Override
+    public java.util.Iterator iterator() {
+        return null;
+    }
+
+
+    private static class Element {
+        Object item;
+        Element next;
+    }
+
+    private class SimpleIteratorImpl implements Iterator {
+
+        @Override
+        public boolean hasNext() {
+            return false;
+        }
+
+        @Override
+        public Object next() {
+            return null;
+        }
+    }
 }
