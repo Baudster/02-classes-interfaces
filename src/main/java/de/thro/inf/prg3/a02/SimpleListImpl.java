69b191d800af98cc86c6da4150fabb04e2903d7a
baudster <bauderse68422@th-nuernberg.de>
2019-04-04 09:26:18 +0200

    add und size funktionen implementiert

--- src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
+++ src/main/java/de/thro/inf/prg3/a02/SimpleListImpl.java
@@ -13,16 +13,50 @@ public class SimpleListImpl implements SimpleList, Iterable {
     // SimpleList Methoden
     @Override
     public void add(Object o) {
+        Element element = new Element();
+        element.item = o;
+        element.next = null;
 
+        if (head == null)
+        {
+            head = element;
+        }
+        else
+        {
+            Element pointer;
+            pointer = head;
+            while (pointer.next != null)
+            {
+                pointer = pointer.next;
+            }
+            pointer.next = element;
+        }
     }
 
     @Override
-    public int size() {
-        return 0;
+    public int size()
+    {
+        Element pointer = head;
+        if (pointer == null)
+        {
+            return 0;
+        }
+
+        int amount = 1;
+
+        while (pointer.next != null)
+        {
+            pointer = pointer.next;
+            amount++;
+        }
+        return amount;
+
     }
 
     @Override
     public SimpleList filter(SimpleFilter filter) {
+        // neue Liste anlegen
+
         return null;
     }
 
@@ -40,8 +74,11 @@ public class SimpleListImpl implements SimpleList, Iterable {
 
     private class SimpleIteratorImpl implements Iterator {
 
+        Element element = head;
+
         @Override
-        public boolean hasNext() {
+        public boolean hasNext()
+        {
             return false;
         }
 
